3cb12f482bb514a3571c7c8989b3aa25ea53be23
Margaret Kiama <maggie.m.maina@gmail.com>
2020-11-25 01:34:36 +0000

    Add files via upload

---
+++ python 1 Github.py
@@ -0,0 +1,21 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:21:23) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(maggie)
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    print(maggie)
+NameError: name 'maggie' is not defined
+>>> print(7)
+7
+>>> print("maggie)")
+maggie)
+>>> print(7*7)
+49
+>>> print(7**"2)
+      
+SyntaxError: EOL while scanning string literal
+>>> print(2**5)
+32
+>>> print(7**2)
+49
+>>> 
